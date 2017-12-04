ed48842e3c21110b8dac435400b41c4c9a0fafb9
Cole Abbeduto <abbedutocg@msoe.edu>
2017-12-04 10:47:12 -0600

    It broke?

--- simulator.js
+++ simulator.js
@@ -1,7 +1,24 @@
 const io = require('socket.io-client');
-const socket = io('http://localhost:3000');
-//const socket = io('http://138.197.98.186:3000');
+/*const prompt = require('prompt');
 
+console.log('0- localhost, 1- Digital Ocean');
+prompt.get(['choice'], (err, result) => {
+    let url;
+    if (result.choice === "0")
+        url = 'http://localhost:3000';
+    else if (result.choice === "1")
+        url = 'http://138.197.98.186:3000';
+
+    start(io(url));
+});
+
+function start(socket) {
+    setInterval(() => {
+        socket.emit('newData', createData());
+    }, 250);
+}*/
+
+const socket = io('http://localhost:3000');
 
 setInterval(() => {
     socket.emit('newData', createData());
