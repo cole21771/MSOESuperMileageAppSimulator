54c8b117557bb0a7a0177f1e0eef4621f23c5e74
Cole Abbeduto <abbedutocg@msoe.edu>
2017-12-05 09:26:19 -0600

    Added a message indicating the number of variables that are being send and where they are being sent to

--- simulator.js
+++ simulator.js
@@ -1,18 +1,21 @@
 const io = require('socket.io-client');
 const prompt = require('prompt');
+const numVars = 6;
 
 console.log('0- localhost, 1- Digital Ocean');
 prompt.get(['choice'], (err, result) => {
-    let socket;
-    if (result.choice === "0")
-        socket = io('http://localhost:3000');
-    else if (result.choice === "1")
-        socket = io('http://138.197.98.186:3000');
+    let url;
+    if (result['choice'] === "0")
+        url = 'http://localhost:3000';
+    else if (result['choice'] === "1")
+        url = 'http://138.197.98.186:3000';
 
-    start(socket);
+    console.log('Sending', numVars, 'random numbers to', url);
+    start(url);
 });
 
-function start(socket) {
+function start(url) {
+    const socket = io(url);
     setInterval(() => {
         socket.emit('newData', createData());
     }, 250);
@@ -20,7 +23,7 @@ function start(socket) {
 
 function createData () {
     let array = [];
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < numVars; i++) {
         array.push(Math.random() * 10);
     }
 
