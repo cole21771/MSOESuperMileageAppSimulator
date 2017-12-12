9ab3588279c9ecca6d0ef27480708bb17eebbb9a
Cole Abbeduto <abbedutocg@msoe.edu>
2017-12-11 21:38:50 -0600

    Updated the simulator to be easier to add new servers.

--- simulator.js
+++ simulator.js
@@ -1,20 +1,47 @@
+class Server {
+    constructor(name, ip, port) {
+        this.name = name;
+        this.ip = ip;
+        this.port = port;
+    }
+
+    getURL() {
+        return this.ip + ":" + this.port;
+    }
+}
+
 const io = require('socket.io-client');
 const prompt = require('prompt');
 const numVars = 6;
+let servers;
 
-console.log('0- localhost, 1- Digital Ocean');
-prompt.get(['choice'], (err, result) => {
-    let url;
-    if (result['choice'] === "0")
-        url = 'http://localhost:3000';
-    else if (result['choice'] === "1")
-        url = 'http://138.197.98.186:3000';
+function setupServers() {
+    servers = [
+        new Server('localhost', 'http://localhost', '3000'),
+        new Server('Digital Ocean', 'http://138.197.98.186:3000', '3000'),
+        new Server('Apartment Desktop', 'http://65.29.164.69', '3000')
+    ];
+}
 
-    console.log('Sending', numVars, 'random numbers to', url);
-    start(url);
-});
+function promptForServer() {
+    let options = "";
+
+    servers.forEach((server, index, array) => {
+        options += index + "- " + server.name + (index === array.size - 1 ? "" : ", ");
+    });
+
+    console.log(options);
+
+    prompt.get(['choice'], (err, result) => {
+        start(servers[result['choice']].getURL());
+    });
+}
+
+setupServers();
+promptForServer();
 
 function start(url) {
+    console.log('Sending', numVars, 'random numbers to', url);
     const socket = io(url);
     setInterval(() => {
         socket.emit('newData', createData());
@@ -22,7 +49,8 @@ function start(url) {
 }
 
 let lastArray = new Array(numVars).fill(0);
-function createData () {
+
+function createData() {
     let array = [];
     for (let i = 0; i < numVars; i++) {
         array.push(Math.random() * lastArray[i] + Math.random() * (lastArray[i] / 1.3) + Math.random());
